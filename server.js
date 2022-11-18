d8d5a1126c78c297d54e5ade28fea4c0adcc947c
Jackie Cheng <pcheng1@cs.ualberta.ca>
2022-11-18 02:02:22 -0700

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const http = require("http");
 const ParcelBundler = require("parcel-bundler");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 const PUBLIC_DIR = "public";
 const STATIC_DIR = "static";
 
